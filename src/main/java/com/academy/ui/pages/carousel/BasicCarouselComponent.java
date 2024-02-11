fadcffb05cb88e37fb9906a6640d9dd0e4675203
Yuliia Ukrainets <ulaukrainec@gmail.com>
2024-02-11 15:20:31 +0200

    Added handling for the case when a user enters invalid index in the getSlickDotByIndex method

--- src/main/java/com/academy/ui/pages/carousel/BasicCarouselComponent.java
+++ src/main/java/com/academy/ui/pages/carousel/BasicCarouselComponent.java
@@ -50,9 +50,12 @@ public class BasicCarouselComponent extends BaseComponent {
             return SlickDots;
         }
 
-        public WebElement getSlickDotByIndex ( int index){
+    public WebElement getSlickDotByIndex (int index){
+        if(index >=0 && index < this.getSlickDots().size()) {
             return this.getSlickDots().get(index);
         }
+        throw new IllegalArgumentException("The index must be in the range between 0 and " + (this.getSlickDots().size()-1)+", inclusive");
+    }
 
         public void clickSlickDotByIndex ( int index){
             this.getSlickDotByIndex(index).click();

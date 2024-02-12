110226a3932d8b54d5d9f6899209d2b1756a7dec
Yuliia Ukrainets <ulaukrainec@gmail.com>
2024-02-12 14:11:24 +0200

    Added a button and a heading, which do not belong to the slider, to the CarouselCardComponenet

--- src/main/java/com/academy/ui/pages/carousel/CarouselCardComponent.java
+++ src/main/java/com/academy/ui/pages/carousel/CarouselCardComponent.java
@@ -1,10 +1,28 @@
 package com.academy.ui.pages.carousel;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class CarouselCardComponent extends BasicCarouselComponent{
   public  CarouselCardComponent(WebDriver driver, WebElement rootElement, String rootElementClass) {
         super(driver, rootElement, rootElementClass);
+    }
+    protected WebElement CarouselCardHeading;
+  protected WebElement CarouselCardAllClubsButton;
+    public WebElement getCarouselCardHeading(){
+        if(CarouselCardHeading == null){
+            CarouselCardHeading = rootElement.findElement(By.xpath("//div[contains(@class,\"categories-header\")]/h2"));
+        }
+        return CarouselCardHeading;
+    }
+    public WebElement getCarouselCardAllClubsButton(){
+        if(CarouselCardAllClubsButton == null){
+            CarouselCardAllClubsButton = rootElement.findElement(By.xpath("//div[contains(@class,\"categories-header\")]/a/button"));
+        }
+        return CarouselCardAllClubsButton;
+    }
+    public void clickCarouselCardAllClubsButton(){
+        this.getCarouselCardAllClubsButton().click();
     }
 }

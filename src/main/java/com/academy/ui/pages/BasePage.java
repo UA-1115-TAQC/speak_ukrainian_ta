eba9fc9b5ee6cb432783df7ddf37dfbdfae78269
Yuliia Ukrainets <ulaukrainec@gmail.com>
2024-02-13 13:43:02 +0200

    Moved the advancedSearch component to a separate base page with advancedSearch

--- src/main/java/com/academy/ui/pages/BasePage.java
+++ src/main/java/com/academy/ui/pages/BasePage.java
@@ -9,11 +9,10 @@ import org.openqa.selenium.WebElement;
 
 public abstract class BasePage extends Base {
     public HeaderComponent header;
-    public AdvancedSearchHeaderComponent advancedSearchHeaderComponent;
+
     public BasePage(WebDriver driver) {
         super(driver);
         this.header = getHeader();
-        this.advancedSearchHeaderComponent = getAdvancedSearchHeaderComponent();
     }
 
     public HeaderComponent getHeader() {
@@ -23,14 +22,6 @@ public abstract class BasePage extends Base {
         }
         return header;
     }
-    public AdvancedSearchHeaderComponent getAdvancedSearchHeaderComponent() {
-        if (advancedSearchHeaderComponent == null) {
-            WebElement node = driver.findElement(By.xpath("//div[contains(@class, \"lower-header-box\")]"));
-            advancedSearchHeaderComponent = new AdvancedSearchHeaderComponent(driver, node);
-        }
-        return advancedSearchHeaderComponent;
-    }
-
 
     public void openURL(String url) {
         driver.get(url);
--- src/main/java/com/academy/ui/pages/HomePage.java
+++ src/main/java/com/academy/ui/pages/HomePage.java
@@ -2,7 +2,7 @@ package com.academy.ui.pages;
 
 import org.openqa.selenium.WebDriver;
 
-public class HomePage extends BasePage{
+public class HomePage extends BasePageWithAdvancedSearch{
 
     public HomePage(WebDriver driver) {
         super(driver);

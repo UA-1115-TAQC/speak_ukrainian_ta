9fd72b7a2ae9e600373a2163392a3265f611d071
Anastasiia <anastasiiak55@gmail.com>
2024-02-13 21:57:38 +0200

    implemented SwitchPaginationComponent class

--- src/main/java/com/academy/ui/pages/ClubsPage.java
+++ src/main/java/com/academy/ui/pages/ClubsPage.java
@@ -4,26 +4,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ClubsPage extends BasePage{
     private final String SEARCH_CLUB_HEADER_ROOT_XPATH =
             "//div[contains(@class, 'lower-header-box')]";
-    private final String CLUB_CARD_LIST_XPATH =
-            "//div[contains(@class,'content-clubs-list')]";
     private final String SWITCH_PAGINATION_ROOT_XPATH =
             "//ul[contains(@class,'ant-pagination') and contains(@class,'pagination')]";
     private AdvancedSearchClubHeaderComponent advancedSearchClubHeader;
+    private SwitchPaginationComponent switchPagination;
 
     public ClubsPage(WebDriver driver) {
         super(driver);
         WebElement clubSearchHeaderRootElement = this.driver.findElement(By.xpath(SEARCH_CLUB_HEADER_ROOT_XPATH));
         advancedSearchClubHeader = new AdvancedSearchClubHeaderComponent(this.driver, clubSearchHeaderRootElement);
+
+        WebElement switchPaginationRootElement = this.driver.findElement(By.xpath(SWITCH_PAGINATION_ROOT_XPATH));
+        switchPagination = new SwitchPaginationComponent(this.driver, switchPaginationRootElement);
     }
 
     public AdvancedSearchClubHeaderComponent getAdvancedSearchClubHeader(){
         return advancedSearchClubHeader;
+    }
+
+    public SwitchPaginationComponent getSwitchPagination(){
+        return switchPagination;
     }
 
 }
---
+++ src/main/java/com/academy/ui/pages/SwitchPaginationComponent.java
@@ -0,0 +1,113 @@
+package com.academy.ui.pages;
+
+import org.openqa.selenium.*;
+import java.util.List;
+
+public class SwitchPaginationComponent extends BaseComponent{
+    protected WebElement previousItem;
+    protected WebElement nextItem;
+    protected List<WebElement> paginationItems;
+
+    public SwitchPaginationComponent(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+
+    public WebElement getPrevious(){
+        if (previousItem == null) {
+            previousItem = rootElement.findElement(By.xpath(
+                    ".//li[contains(@class,'ant-pagination-prev')]"));
+        }
+        return previousItem;
+    }
+
+    public WebElement getNext(){
+        if (nextItem == null) {
+            nextItem = rootElement.findElement(By.xpath(
+                    ".//li[contains(@class,'ant-pagination-next')]"));
+        }
+        return nextItem;
+    }
+
+    private void createPagItemsList(){
+        paginationItems = rootElement.findElements(By.xpath(
+                ".//li[contains(@class, 'ant-pagination-item') or contains(@class, 'ant-pagination-jump-')]"));
+    }
+
+    public List<WebElement> getPagItems(){
+        if(paginationItems == null){
+            createPagItemsList();
+        }
+        return paginationItems;
+    }
+
+    public WebElement getPagItemByTitle(String pageNum){
+        getPagItems();
+        for(WebElement pagItem : paginationItems){
+            String str = pagItem.getDomAttribute("title");
+            if(str.equals(pageNum)){
+                return pagItem;
+            }
+        }
+        return null;
+    }
+
+    public boolean isPagItemActive(String pageNum){
+        getPagItems();
+        WebElement pagItem = getPagItemByTitle(pageNum);
+        if(pagItem != null){
+            String str = pagItem.getDomAttribute("class");
+            if(str.contains("ant-pagination-item-active")){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void clickPrevious(){
+        getPrevious();
+        WebElement previousButton = previousItem.findElement(By.xpath(
+                ".//button"));
+        if(previousButton.isEnabled()) {
+            previousButton.click();
+        }
+    }
+
+    public void clickNext(){
+        getNext();
+        WebElement nextButton = nextItem.findElement(By.xpath(
+                ".//button"));
+        if(nextButton.isEnabled()) {
+            nextButton.click();
+        }
+    }
+
+    public void clickPagItem(WebElement item){
+            item.click();
+            createPagItemsList();
+    }
+
+    public void clickPagItemByNum(String pageNum){
+        getPagItems();
+        WebElement pagItem = getPagItemByTitle(pageNum);
+        if(pagItem != null) {
+            clickPagItem(pagItem);
+        }
+    }
+
+    public void clickPrevFivePages(){
+        getPagItems();
+        WebElement pagItem = getPagItemByTitle("Previous 5 Pages");
+        if(pagItem != null) {
+            clickPagItem(pagItem);
+        }
+    }
+
+    public void clickNextFivePages(){
+        getPagItems();
+        WebElement pagItem = getPagItemByTitle("Next 5 Pages");
+        if(pagItem != null) {
+            clickPagItem(pagItem);
+        }
+    }
+
+}

c403e24e910dbf064aba7d9b58881e88146629b3
Yuliia Ukrainets <ulaukrainec@gmail.com>
2024-02-13 15:06:39 +0200

    Minor refactoring of the  properties names

--- src/main/java/com/academy/ui/pages/HomePage.java
+++ src/main/java/com/academy/ui/pages/HomePage.java
@@ -3,39 +3,38 @@ package com.academy.ui.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 public class HomePage extends BasePageWithAdvancedSearch {
 
-    protected WebElement ChallengeDescriptionHeading;
-    protected WebElement ChallengeDescriptionText;
-    protected WebElement ChallengeFindOutMoreButton;
-    protected WebElement ChallengeImage;
-    protected WebElement SpeakingClubHeading;
-    protected WebElement SpeakingClubImage;
-    protected String ChallengeDescriptionDivPath = "//div[contains(@class,\"challenge-description\")]";
+    protected WebElement challengeDescriptionHeading;
+    protected WebElement challengeDescriptionText;
+    protected WebElement challengeFindOutMoreButton;
+    protected WebElement challengeImage;
+    protected WebElement speakingClubHeading;
+    protected WebElement speakingClubImage;
+    protected String challengeDescriptionDivPath = "//div[contains(@class,\"challenge-description\")]";
     public HomePage(WebDriver driver) {
         super(driver);
     }
 
     public WebElement getChallengeDescriptionHeading() {
-        if (ChallengeDescriptionHeading == null) {
-            ChallengeDescriptionHeading = driver.findElement(By.xpath(ChallengeDescriptionDivPath + "/h2"));
+        if (challengeDescriptionHeading == null) {
+            challengeDescriptionHeading = driver.findElement(By.xpath(challengeDescriptionDivPath + "/h2"));
         }
-        return ChallengeDescriptionHeading;
+        return challengeDescriptionHeading;
     }
 
     public WebElement getChallengeDescriptionText() {
-        if (ChallengeDescriptionText == null) {
-            ChallengeDescriptionText = driver.findElement(By.xpath(ChallengeDescriptionDivPath + "/span"));
+        if (challengeDescriptionText == null) {
+            challengeDescriptionText = driver.findElement(By.xpath(challengeDescriptionDivPath + "/span"));
         }
-        return ChallengeDescriptionText;
+        return challengeDescriptionText;
     }
 
     public WebElement getChallengeFindOutMoreButton() {
-        if (ChallengeFindOutMoreButton == null) {
-            ChallengeFindOutMoreButton = driver.findElement(By.xpath(ChallengeDescriptionDivPath + "//button"));
+        if (challengeFindOutMoreButton == null) {
+            challengeFindOutMoreButton = driver.findElement(By.xpath(challengeDescriptionDivPath + "//button"));
         }
-        return ChallengeFindOutMoreButton;
+        return challengeFindOutMoreButton;
     }
 
     public void clickChallengeFindOutMoreButton() {
@@ -43,17 +42,17 @@ public class HomePage extends BasePageWithAdvancedSearch {
     }
 
     public WebElement getChallengeImage() {
-        if (ChallengeImage == null) {
-            ChallengeImage = driver.findElement(By.xpath("//div[contains(@class,\"about-challenge\")]//img"));
+        if (challengeImage == null) {
+            challengeImage = driver.findElement(By.xpath("//div[contains(@class,\"about-challenge\")]//img"));
         }
-        return ChallengeImage;
+        return challengeImage;
     }
 
     public WebElement getSpeakingClubHeading() {
-        if (SpeakingClubHeading == null) {
-            SpeakingClubHeading = driver.findElement(By.xpath("//div[contains(@class,\"speakingclub-description\")]//h2"));
+        if (speakingClubHeading == null) {
+            speakingClubHeading = driver.findElement(By.xpath("//div[contains(@class,\"speakingclub-description\")]//h2"));
         }
-        return SpeakingClubHeading;
+        return speakingClubHeading;
     }
 
     public void clickSpeakingClubHeading() {
@@ -61,9 +60,13 @@ public class HomePage extends BasePageWithAdvancedSearch {
     }
 
   public WebElement getSpeakingClubImage() {
-        if (SpeakingClubImage == null) {
-            SpeakingClubImage = driver.findElement(By.xpath("//img[contains(@class,\"banner-image\")]"));
+        if (speakingClubImage == null) {
+            speakingClubImage = driver.findElement(By.xpath("//img[contains(@class,\"banner-image\")]"));
         }
-        return SpeakingClubImage;
+        return speakingClubImage;
+    }
+
+    public void clickSpeakingClubImage() {
+            this.getSpeakingClubImage().click();
     }
 }

10f040e34399fca38c3ee7458444b54b047d2ced
Olena <yelenastankevych@gmail.com>
2024-02-14 11:52:12 +0200

    moved some logic from PaginationComponent to AllNewsPage

--- src/main/java/com/academy/ui/pages/AllNewsPage.java
+++ src/main/java/com/academy/ui/pages/AllNewsPage.java
@@ -6,6 +6,7 @@ import com.academy.ui.pages.component.PaginationComponent;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import static org.openqa.selenium.By.xpath;
 
@@ -37,5 +38,24 @@ public class AllNewsPage extends BasePage {
             paginationComponent = new PaginationComponent(driver);
         }
         return paginationComponent;
+    }
+
+    public void goToPage(int pageNumber) {
+        getPaginationComponent().getPageByNumber(pageNumber)
+                .ifPresent(WebElement::click);
+    }
+
+    //this is just example. Because there may be a situation when there are two ... (one for the previous page and one for the next one)
+    public void clickThreeDotPagination() {
+        getPaginationComponent().getThreeDotPagination()
+                .ifPresent(WebElement::click);
+    }
+
+    public void clickPreviousPage() {
+        getPaginationComponent().getPreviousPageArrow().click();
+    }
+
+    public void clickNextPage() {
+        getPaginationComponent().getNextPageArrow().click();
     }
 }
--- src/main/java/com/academy/ui/pages/component/PaginationComponent.java
+++ src/main/java/com/academy/ui/pages/component/PaginationComponent.java
@@ -10,43 +10,35 @@ import static org.openqa.selenium.By.xpath;
 
 //this class is common for AllNewsPage and ClubsPage
 public class PaginationComponent extends Base {
-    private final WebElement root;
     private List<WebElement> pagePagination;
     private WebElement previousPageArrow;
     private WebElement nextPageArrow;
     public PaginationComponent(WebDriver driver) {
         super(driver);
-        root = driver.findElement(xpath("//ul[contains(@class, 'ant-pagination')]"));
     }
 
     public List<WebElement> getPagePagination() {
-        return root
-                .findElements(xpath(".//li[contains(@class, 'ant-pagination-item') or contains(@class, 'ant-pagination-jump')]"));
+        return driver
+                .findElements(xpath("//li[contains(@class, 'ant-pagination-item') or contains(@class, 'ant-pagination-jump')]"));
     }
 
     public WebElement getPreviousPageArrow() {
-        return root.findElement(xpath(".//li[contains(@title, 'Previous Page')]"));
+        return driver.findElement(xpath("//li[contains(@title, 'Previous Page')]"));
     }
 
     public WebElement getNextPageArrow() {
-        return root.findElement(xpath(".//li[contains(@title, 'Next Page')]"));
+        return driver.findElement(xpath("//li[contains(@title, 'Next Page')]"));
     }
 
-    //this is just example. Because there may be a situation when there are two ... (one for the previous page and one for the next one)
-    public void clickThreeDotPagination() {
-        getThreeDotPagination()
-                .ifPresent(WebElement::click);
+    public Optional<WebElement> getPageByNumber(int pageNumber) {
+        return pageNumber > 0 && pageNumber < getPagePagination().size() ?
+                Optional.of(getPagePagination().get(pageNumber - 1)) : Optional.empty();
     }
 
-    public void goToPage(int pageNumber) {
-        if (pageNumber > 0 && pageNumber < getPagePagination().size()) {
-            getPagePagination().get(pageNumber - 1).click();
-        }
-    }
-
-    private Optional<WebElement> getThreeDotPagination() {
+    public Optional<WebElement> getThreeDotPagination() {
         return getPagePagination()
                 .stream()
                 .filter(webElement -> webElement.getAttribute("class").contains("jump-next ant-pagination"))
                 .findFirst();
-    }}
+    }
+}

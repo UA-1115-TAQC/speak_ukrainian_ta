8bddef3e7d2a7d71601a77fe3c40c0b341d81e4c
Oksanka Syrota <oksanochka.v@gmail.com>
2024-02-15 11:08:55 +0200

    Added addClubButtonClick method to HeaderComponent

--- src/main/java/com/academy/ui/pages/header/HeaderComponent.java
+++ src/main/java/com/academy/ui/pages/header/HeaderComponent.java
@@ -1,15 +1,30 @@
 package com.academy.ui.pages.header;
 
+
+import com.academy.ui.components.AddClubPopUpComponent.AddClubPopUpComponent;
 import com.academy.ui.components.BaseComponent;
 import com.academy.ui.pages.AllNewsPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HeaderComponent extends BaseComponent {
     protected WebElement profileMenuButton;
     protected WebElement groupButton;
     protected WebElement newsButton;
+    public AddClubPopUpComponent addClubPopUp;
+
+    @FindBy(xpath = "//span[contains(@class,'avatarIfLogin')]")
+    private WebElement isLoggedIn;
+
+    @FindBy(xpath = "//button[contains(@class,'add-club-button')]")
+    protected WebElement addClubButton;
+
 
     public HeaderComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
@@ -41,4 +56,13 @@ public class HeaderComponent extends BaseComponent {
         getNewsButton().click();
         return new AllNewsPage(driver);
     }
+
+    public void addClubButtonClick() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(e -> isLoggedIn.isDisplayed());
+        addClubButton.click();
+        addClubPopUp = new AddClubPopUpComponent(driver);
+    }
+
+
 }

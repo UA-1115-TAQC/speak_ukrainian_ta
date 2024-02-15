924dd5c1ca15ae2f6493b29416171b775f4a6214
Oksanka Syrota <oksanochka.v@gmail.com>
2024-02-15 11:02:00 +0200

    Refactored BasePopUp class with FindBy annotations

--- src/main/java/com/academy/ui/components/BasePopUp.java
+++ src/main/java/com/academy/ui/components/BasePopUp.java
@@ -3,14 +3,18 @@ package com.academy.ui.components;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public abstract class BasePopUp extends BaseComponent {
-    protected WebElement closeButton;
+
+    @FindBy(xpath = "//div[contains(@class,'ant-modal-centered') and not(contains(@style,'display'))]/descendant::button[@class='ant-modal-close']")
+    private WebElement closeButton;
 
     public BasePopUp(WebDriver driver, String className) {
         super(driver, driver.findElement(By.xpath("//div[contains(@class,'" + className + "')]")));
+
     }
 
     public boolean waitPopUpOpen(java.time.Duration timeout) {
@@ -18,14 +22,7 @@ public abstract class BasePopUp extends BaseComponent {
         return wait.until(e -> rootElement.isDisplayed());
     }
 
-    public WebElement getCloseButton() {
-        if (closeButton == null) {
-            closeButton = driver.findElement(By.xpath("//div[contains(@class,'ant-modal-centered') and not(contains(@style,'display'))]/descendant::button[@class='ant-modal-close']"));
-        }
-        return closeButton;
-    }
-
     public void clickCloseButton() {
-        getCloseButton().click();
+        closeButton.click();
     }
 }

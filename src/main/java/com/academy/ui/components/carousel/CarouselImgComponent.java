14f0a6f0219b71d0885185d562d822890f340c2f
Yuliia Ukrainets <ulaukrainec@gmail.com>
2024-02-15 12:27:35 +0200

    Implemented CarouselImgComponent

--- src/main/java/com/academy/ui/components/carousel/CarouselImgComponent.java
+++ src/main/java/com/academy/ui/components/carousel/CarouselImgComponent.java
@@ -1,10 +1,50 @@
 package com.academy.ui.components.carousel;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
 
 public class CarouselImgComponent extends BasicCarouselComponent {
-    public CarouselImgComponent(WebDriver driver, WebElement rootElement) {
+    protected HashMap<Integer, WebElement> switchingCarouselImgCards;
+    public  CarouselImgComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
+    }
+    public HashMap<Integer, WebElement>getCarouselImgCards(){
+        if(switchingCarouselImgCards == null){
+            switchingCarouselImgCards = new HashMap<>();
+            List<WebElement> imgCards = this.getSliderContainer().findElements(By.xpath(".//div[contains(@class,\"slick-slide\")]"));
+            for(WebElement imgCard:imgCards){
+                int dataIndex = Integer.parseInt(imgCard.getAttribute("data-index"));
+                if(dataIndex >= 0 && dataIndex <= 2){
+                    switchingCarouselImgCards.put(dataIndex, imgCard);
+                }
+            }
+        }
+        return switchingCarouselImgCards;
+    }
+    public CarouselImgCard getCarouselImgCardByDataIndex(int dataIndex){
+        if(dataIndex >= 0 && dataIndex < getCarouselImgCards().size()) {
+            WebElement imgCard = this.getCarouselImgCards().get(dataIndex);
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofMinutes(1));
+            wait.until(ExpectedConditions.visibilityOf(imgCard));
+            return new CarouselImgCard(imgCard);
+        }
+        throw new IllegalArgumentException("The index must be in the range from 0 to "+(getCarouselImgCards().size()-1)+", inclusive.");
+    }
+    public CarouselImgCard getActiveCarouselImgCard()  {
+        int dataIndex = 0;
+        for (int i = 0; i < this.getCarouselImgCards().size(); i++){
+            if( getCarouselImgCards().get(i).getAttribute("class").contains("active")){
+                dataIndex=i;
+            }
+
+        }
+        return new CarouselImgCard( this.getCarouselImgCards().get(dataIndex));
     }
 }

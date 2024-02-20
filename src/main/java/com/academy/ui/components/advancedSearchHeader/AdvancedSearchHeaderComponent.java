71077e9fda01e7bcfa86043531fa2f40ab0370e4
Liubomyr Halamaha <galamagal86@gmail.com>
2024-02-20 13:03:40 +0200

    update

--- src/main/java/com/academy/ui/components/advancedSearchHeader/AdvancedSearchHeaderComponent.java
+++ src/main/java/com/academy/ui/components/advancedSearchHeader/AdvancedSearchHeaderComponent.java
@@ -3,7 +3,6 @@ package com.academy.ui.components.advancedSearchHeader;
 import com.academy.ui.components.BaseComponent;
 import com.academy.ui.pages.ClubsPage;
 import lombok.Getter;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -24,34 +23,29 @@ public class AdvancedSearchHeaderComponent extends BaseComponent {
     protected WebElement searchInputCloseButton;
     @FindBy(xpath = "//div[contains(@class, \"rc-virtual-list-holder-inner\")]")
     protected WebElement advancedSearchTooltipNode;
-    @FindBy(xpath="//span[@aria-label=\"close-circle\"]")
+    @FindBy(xpath = "//span[@aria-label=\"close-circle\"]")
     protected WebElement selectionSearchCloseButton;
-    protected AdvancedSearchTooltip advancedSearchTooltip;
+
 
     public AdvancedSearchHeaderComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
-    }
-    public AdvancedSearchHeaderComponent setTextSelectionSearchInputField(String text) {
-        this.selectionSearchInputField.sendKeys(text);
-        return this;
     }
 
     public String getTextSelectionSearchInputField() {
         return this.getSelectionSearchInputField().getAttribute("value");
     }
 
-    public void setTextSelectionSearchInputField(String text) {
+    public AdvancedSearchHeaderComponent setTextSelectionSearchInputField(String text) {
         this.selectionSearchInputField.sendKeys(text);
+        return this;
     }
 
     public AdvancedSearchTooltip clickSelectionSearchInputField() {
         this.getSelectionSearchInputField().click();
-        return advancedSearchTooltip = new AdvancedSearchTooltip(driver, getAdvancedSearchTooltipNode());
+        return new AdvancedSearchTooltip(driver, getAdvancedSearchTooltipNode());
     }
 
     //there is a bug - when you click on this icon after entering some text, nothing happens
-    public void clickSearchIcon() {
-//there is a bug - when you click on this icon after entering some text, nothing happens
     public AdvancedSearchHeaderComponent clickSearchIcon() {
         this.searchIcon.click();
         return this;
@@ -61,14 +55,15 @@ public class AdvancedSearchHeaderComponent extends BaseComponent {
         this.advancedSearchIcon.click();
         return new ClubsPage(driver);
     }
-    public AdvancedSearchHeaderComponent clickSelectionSearchCloseButton(){
+
+    public AdvancedSearchHeaderComponent clickSelectionSearchCloseButton() {
         try {
             if (getTextSelectionSearchInputField() != null) {
                 getSelectionSearchCloseButton().click();
                 return this;
             }
             throw new Error("You haven't entered any text in the search field");
-        } catch(Error error){
+        } catch (Error error) {
             System.out.println(error.getMessage());
             return null;
         }

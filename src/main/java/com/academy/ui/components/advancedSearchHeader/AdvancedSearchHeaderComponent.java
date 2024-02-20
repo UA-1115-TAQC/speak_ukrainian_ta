1fbed397ce7674a55182f0a5837e0994caa03803
Liubomyr Halamaha <galamagal86@gmail.com>
2024-02-20 12:09:50 +0200

    fix init pagefactori in BaseComponent

--- src/main/java/com/academy/ui/components/advancedSearchHeader/AdvancedSearchHeaderComponent.java
+++ src/main/java/com/academy/ui/components/advancedSearchHeader/AdvancedSearchHeaderComponent.java
@@ -7,8 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
+
 @Getter
 public class AdvancedSearchHeaderComponent extends BaseComponent {
     @FindBy(xpath = "//h2[@class='city-name']")
@@ -28,12 +27,13 @@ public class AdvancedSearchHeaderComponent extends BaseComponent {
     public AdvancedSearchHeaderComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
     }
-    public void setTextSelectionSearchInputField(String text) {
-        this.selectionSearchInputField.sendKeys(text);
-    }
 
     public String getTextSelectionSearchInputField() {
         return this.getSelectionSearchInputField().getAttribute("value");
+    }
+
+    public void setTextSelectionSearchInputField(String text) {
+        this.selectionSearchInputField.sendKeys(text);
     }
 
     public AdvancedSearchTooltip clickSelectionSearchInputField() {
@@ -41,7 +41,8 @@ public class AdvancedSearchHeaderComponent extends BaseComponent {
         WebElement node = driver.findElement(By.xpath("//div[contains(@class, \"rc-virtual-list-holder-inner\")]"));
         return advancedSearchTooltip = new AdvancedSearchTooltip(driver, node);
     }
-//there is a bug - when you click on this icon after entering some text, nothing happens
+
+    //there is a bug - when you click on this icon after entering some text, nothing happens
     public void clickSearchIcon() {
         this.searchIcon.click();
     }

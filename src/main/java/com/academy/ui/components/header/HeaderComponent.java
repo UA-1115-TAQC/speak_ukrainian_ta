0721239b9aa3837b8ff5afa906051a77a570df46
Liubomyr Halamaha <galamagal86@gmail.com>
2024-02-22 14:02:51 +0200

    fix

--- src/main/java/com/academy/ui/components/header/HeaderComponent.java
+++ src/main/java/com/academy/ui/components/header/HeaderComponent.java
@@ -7,7 +7,6 @@ import com.academy.ui.components.header.headerMenuComponent.GuestMenuComponent;
 import com.academy.ui.components.loginPopUpComponent.LoginPopupComponent;
 import com.academy.ui.pages.AllNewsPage;
 import lombok.Getter;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,16 +17,13 @@ import java.time.Duration;
 
 @Getter
 public class HeaderComponent extends BaseComponent {
+    public LoginPopupComponent loginPopupComponent;
     @FindBy(xpath = ".//div[contains(@class, 'user-profile')]")
     protected WebElement profileMenuButton;
     @FindBy(xpath = "//*[@id=\"root\"]/div/header/div[2]/ul/li[1]")
     protected WebElement groupButton;
     @FindBy(xpath = "//a[@href='/news']")
     protected WebElement newsButton;
-
-    @FindBy(xpath = "//span[contains(@class,'avatarIfLogin')]")
-    private WebElement isLoggedIn;
-
     @FindBy(xpath = "//button[contains(@class,'add-club-button')]")
     protected WebElement addClubButton;
 
@@ -43,14 +39,14 @@ public class HeaderComponent extends BaseComponent {
     @FindBy(xpath = "//ul[contains(@class, 'ant-dropdown-menu')]")
     protected WebElement profileMenuNode;
 
-
-    public LoginPopupComponent loginPopupComponent;
+    @FindBy(xpath = "//span[contains(@class,'avatarIfLogin')]")
+    private WebElement isLoggedIn;
 
     public HeaderComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
     }
 
-    public HeaderChallengesDropdown clickChallengeButton(){
+    public HeaderChallengesDropdown clickChallengeButton() {
         this.getChallengeButton().click();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         wait.until(ExpectedConditions.visibilityOf(getHeaderChallengeDropdownNode()));
@@ -69,7 +65,7 @@ public class HeaderComponent extends BaseComponent {
         return new AddClubPopUpComponent(driver);
     }
 
-    public GuestMenuComponent openGuestMenu(){
+    public GuestMenuComponent openGuestMenu() {
         profileMenuButton.click();
         return new GuestMenuComponent(driver, profileMenuNode);
     }

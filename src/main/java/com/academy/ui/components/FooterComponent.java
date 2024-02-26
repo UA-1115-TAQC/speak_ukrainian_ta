d31515928b5867c7800bcf59d9d1fe6c8d925785
Nataliia Lazorko <natalia.lazorko.45@gmail.com>
2024-02-26 13:30:50 +0200

    updated foote with xpath

--- src/main/java/com/academy/ui/components/FooterComponent.java
+++ src/main/java/com/academy/ui/components/FooterComponent.java
@@ -1,55 +1,137 @@
 package com.academy.ui.components;
 
-import com.academy.ui.components.BaseComponent;
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
 public class FooterComponent extends BaseComponent {
 
-    @FindBy(xpath = "//div[contains(@class,'footer-logo')]")
-    private WebElement footerLogo;
+    @FindBy(xpath = "./descendant::div[contains(@class,'footer-logo')]")
+    private WebElement logo;
 
-    @FindBy(xpath = "//div[contains(@class,'links')]//a[contains(@href, 'https')]")
-    private List<WebElement> footerSocialLinks;
+    @FindBy(xpath = "./descendant::div[contains(@class,'text')]")
+    private WebElement mottoUnderLogo;
 
-    @FindBy(xpath = "//div[@class=\"footer-partners\"]/div[@class=\"article\"]")
-    private WebElement footerSponsorsArticle;
+    @FindBy(xpath = "./descendant::div[contains(@class,'links')]//a[contains(@href, 'https')]")
+    private List<WebElement> socialLinks;
 
-    @FindBy(xpath = "//div[contains(@class,'sponsors')]//a[contains(@href, 'https')]")
-    private List<WebElement> footerSponsorsLinks;
+    @FindBy(xpath = "./descendant::div[contains(@class,'links')]//a[contains(@href, 'https')][1]")
+    private WebElement facebookLink;
 
-    @FindBy(xpath = "//div[@class=\"footer-donate\"]/div[@class=\"article\"]")
-    private WebElement footerDonateArticle;
-    @FindBy(xpath = "//button[contains(@class,'donate-button')]")
-    private WebElement footerDonateButton;
+    @FindBy(xpath = "./descendant::div[contains(@class,'links')]//a[contains(@href, 'https')][2]")
+    private WebElement youTubeLink;
+
+    @FindBy(xpath = "./descendant::div[contains(@class,'links')]//a[contains(@href, 'https')][3]")
+    private WebElement instagramLink;
+
+    @FindBy(xpath = "./descendant::div[contains(@class,'qubstudio')]")
+    private WebElement copyrightNotice;
+
+    @FindBy(xpath = "./descendant::div[@class=\"footer-partners\"]/div[@class=\"article\"]")
+    private WebElement sponsorsTitle;
+
+    @FindBy(xpath = "./descendant::div[@class='footer-partners']//a/img")
+    private WebElement sponsorsImg;
+
+    @FindBy(xpath = "./descendant::div[contains(@class,'sponsors')]//a[contains(@href, 'https')]")
+    private List<WebElement> sponsorsLinks;
+
+    @FindBy(xpath = "./descendant::div[contains(@class,'sponsors')]//a[contains(@href, 'https')][1]")
+    private WebElement softServeLink;
+
+    @FindBy(xpath = "./descendant::div[contains(@class,'sponsors')]//a[contains(@href, 'https')][2]")
+    private WebElement movaObyednueLink;
+
+    @FindBy(xpath = "./descendant::div[contains(@class,'sponsors')]//a[contains(@href, 'https')][3]")
+    private WebElement ederaLink;
+
+    @FindBy(xpath = "./descendant::div[contains(@class,'sponsors')]//a[contains(@href, 'https')][4]")
+    private WebElement eMovaLink;
+
+    @FindBy(xpath = "./descendant::div[contains(@class,'sponsors')]//a[contains(@href, 'https')][5]")
+    private WebElement krainaFMLink;
+
+    @FindBy(xpath = "./descendant::div[contains(@class,'sponsors')]//a[contains(@href, 'https')][6]")
+    private WebElement ucfLink;
+
+    @FindBy(xpath = "./descendant::div[contains(@class,'sponsors')]//a[contains(@href, 'https')][7]")
+    private WebElement prostirSvobodiLink;
+
+    @FindBy(xpath = "./descendant::div[@class=\"footer-donate\"]/div[@class=\"article\"]")
+    private WebElement donateTitle;
+
+    @FindBy(xpath = "./descendant::div[@class='desc']")
+    private WebElement donateExplanation;
+
+    @FindBy(xpath = "./descendant::button[contains(@class,'donate-button')]")
+    private WebElement donateButton;
 
     public FooterComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
     }
 
-    public void clickOnFooterLogo() {
-        footerLogo.click();
+    public void clickOnLogo() {
+        logo.click();
     }
 
     public List<String> getFooterSocialLinks() {
-        return footerSocialLinks.stream()
+        return socialLinks.stream()
                 .map(link -> link.getAttribute("href"))
                 .collect(Collectors.toList());
     }
 
     public List<String> getFooterSponsorsLinks() {
-        return footerSponsorsLinks.stream()
+        return sponsorsLinks.stream()
                 .map(link -> link.getAttribute("href"))
                 .collect(Collectors.toList());
     }
 
-    public void clickOnFooterDonateButton() {
-        footerDonateButton.click();
+    public void clickOnDonateButton() {
+        donateButton.click();
     }
+
+    public void clickOnFacebookLink() {
+        facebookLink.click();
+    }
+
+    public void clickOnYouTubeLink() {
+        youTubeLink.click();
+    }
+
+    public void clickOnInstagramLink() {
+        instagramLink.click();
+    }
+
+    public void clickOnSoftServeLink() {
+        softServeLink.click();
+    }
+
+    public void clickOnMovaObyednueLink() {
+        movaObyednueLink.click();
+    }
+
+    public void clickOnEderaLink() {
+        ederaLink.click();
+    }
+
+    public void clickOneEMovaLink() {
+        eMovaLink.click();
+    }
+
+    public void clickOnKrainaFMLink() {
+        krainaFMLink.click();
+    }
+
+    public void clickOnUcfLink() {
+        ucfLink.click();
+    }
+
+    public void clickOnProstirSvobodiLink() {
+        prostirSvobodiLink.click();
+    }
+
 }

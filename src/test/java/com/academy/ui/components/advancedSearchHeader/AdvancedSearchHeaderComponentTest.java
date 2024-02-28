e5db778f648c58dc2b4bcec97d3e09e7e8fa52e1
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2024-02-28 13:36:05 +0200

    fixes after first mentor review

--- src/test/java/com/academy/ui/components/advancedSearchHeader/AdvancedSearchHeaderComponentTest.java
+++ src/test/java/com/academy/ui/components/advancedSearchHeader/AdvancedSearchHeaderComponentTest.java
@@ -1,7 +1,6 @@
 package com.academy.ui.components.advancedSearchHeader;
 
 import com.academy.ui.runners.BaseTestRunner;
-import jdk.jfr.Description;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,19 +12,20 @@ public class AdvancedSearchHeaderComponentTest extends BaseTestRunner {
 
     private AdvancedSearchHeaderComponent advancedSearchHeaderComponent;
     private AdvancedSearchTooltip advancedSearchTooltip;
+    private SoftAssert softAssert;
 
     @BeforeMethod
     public void advancedSearchHeaderComponentTest_setUp() {
         advancedSearchHeaderComponent = homePage.getAdvancedSearchHeaderComponent();
+        softAssert = new SoftAssert();
     }
 
     @Test(description = "TUA-316")
-    @Description(value = "Test will pass if you change base URL on https://speak-ukrainian.org.ua/")
     public void advancedSearchHeader_display_categories_and_clubs_ok() {
+        //Test will pass if you change base URL on https://speak-ukrainian.org.ua/
         advancedSearchTooltip = advancedSearchHeaderComponent.clickSelectionSearchInputField();
         HashMap<String, WebElement> categories = advancedSearchTooltip.getCategories();
         HashMap<String, WebElement> clubs = advancedSearchTooltip.getClubs();
-        SoftAssert softAssert = new SoftAssert();
         softAssert.assertFalse(categories.isEmpty());
         softAssert.assertFalse(clubs.isEmpty());
         softAssert.assertAll();

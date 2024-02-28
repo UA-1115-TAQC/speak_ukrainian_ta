9c386202262d562006bbe08b56e76ba3ae283cc4
Nataliia Lazorko <natalia.lazorko.45@gmail.com>
2024-02-28 14:08:50 +0200

    Updated HomePage with scrollToAllClubsButton and README

--- src/test/java/com/academy/ui/homepage_tests/Homepage_Button_AllClubs.java
+++ src/test/java/com/academy/ui/homepage_tests/Homepage_Button_AllClubs.java
@@ -1,7 +1,6 @@
 package com.academy.ui.homepage_tests;
 
 import com.academy.ui.runners.LoginWithUserTestRunner;
-import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,12 +8,8 @@ public class Homepage_Button_AllClubs extends LoginWithUserTestRunner {
 
     @Test(description = "TUA-862")
     public void testbuttonAllClubs(){
-
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView();", homePage.carouselCardComponent.getCarouselCardAllClubsButton());
-
+        homePage.scrollToAllClubsButton();
         homePage.carouselCardComponent.clickCarouselCardAllClubsButton().waitUntilClubsPageIsLoaded(15);
-
         Assert.assertTrue(driver.getCurrentUrl().contains("clubs"));
     }
 }

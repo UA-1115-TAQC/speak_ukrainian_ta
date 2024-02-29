ad502787d337546fdb098f62bbd0d0ca4d5fd6e3
Oksanka Syrota <oksanochka.v@gmail.com>
2024-02-29 21:44:09 +0200

    Implemented abstract container class for steps in the AddCenterPopUPComponent

---
+++ src/main/java/com/academy/ui/components/AddCenterPopUPComponent/AddCenterPopUpContainer.java
@@ -0,0 +1,35 @@
+package com.academy.ui.components.AddCenterPopUPComponent;
+
+import com.academy.ui.components.AddClubPopUpComponent.AddClubPopUpContainer;
+import com.academy.ui.components.BaseComponent;
+import lombok.Getter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+@Getter
+public abstract class AddCenterPopUpContainer extends BaseComponent {
+
+    @FindBy(xpath = "./descendant::div[@class='modal-title']")
+    public WebElement centerTitle;
+
+    @FindBy(xpath = "./descendant::button[contains(@class,'next-btn')]")
+    private WebElement nextStepButton;
+
+    @FindBy(xpath = "./descendant::button[contains(@class,'prev-btn')]")
+    private WebElement prevStepButton;
+
+    public AddCenterPopUpContainer(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+
+    public AddCenterPopUpContainer clickNextStepButton(){
+        nextStepButton.click();
+        return this;
+    }
+
+    public AddCenterPopUpContainer clickPreviousStepButton(){
+        prevStepButton.click();
+        return this;
+    }
+}

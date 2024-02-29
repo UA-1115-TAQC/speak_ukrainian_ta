2c4f6c0e13d8ede6ed16f31b7e2b615e87049bc9
Liubomyr Halamaha <galamagal86@gmail.com>
2024-02-29 13:42:05 +0200

    fix naming

--- src/test/java/com/academy/ui/runners/LogInWithUserTestRunner.java
+++ src/test/java/com/academy/ui/runners/LogInWithUserTestRunner.java
@@ -3,8 +3,8 @@ package com.academy.ui.runners;
 import com.academy.ui.components.loginPopUpComponent.LoginPopupComponent;
 import org.testng.annotations.BeforeMethod;
 
-public class LogInWithUserTestRunner extends BaseTestRunner{
-
+public class LogInWithUserTestRunner extends BaseTestRunner {
+    @BeforeMethod
     public void loginPrecondition() {
         LoginPopupComponent loginForm = homePage
                 .header

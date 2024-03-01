e34c8309464f5addd1d6b04584b9de7178161642
Olena <yelenastankevych@gmail.com>
2024-03-01 12:02:52 +0200

    added fixes in HomePageWithLoggedInUserTest class

--- src/test/java/com/academy/ui/homePage/HomePageWithLoggedInUserTest.java
+++ src/test/java/com/academy/ui/homePage/HomePageWithLoggedInUserTest.java
@@ -1,12 +1,11 @@
 package com.academy.ui.homePage;
 
 import com.academy.ui.runners.LoginWithUserTestRunner;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class HomePageWithLoggedInUserTest extends LoginWithUserTestRunner {
     private SoftAssert softAssert;
@@ -24,41 +23,44 @@ public class HomePageWithLoggedInUserTest extends LoginWithUserTestRunner {
     }
 
     @Test(description = "TUA-861")
-    public void verifyActiveClubsCarouselIndicatorCorrespondCarouselBlock() {
-        final var expectedActiveSlickDotColor = "#fa8c16";
-        final var carouselCardTitleExpected = List.of(
+    public void verifyActiveSlickDotCorrespondsToClubCards() {
+        final var expectedActiveDotColor = "#fa8c16";
+        final var expectedClubTitles = List.of(
                 "Художня студія, мистецтво, дизайн",
                 "Вокальна студія, музика, музичні інструменти",
                 "Акторська майстерність, театр",
                 "Особистісний розвиток");
 
-        var carouselCardComponent = homePage.getCarouselCardComponent();
+        var clubCarousel = homePage.getCarouselCardComponent();
         //check first slick dot in orange color
-        softAssert.assertEquals(carouselCardComponent.getSlickDotColor(carouselCardComponent.getSlickDots().get(0)),
-                        expectedActiveSlickDotColor);
+        softAssert.assertEquals(clubCarousel.getSlickDotColor(clubCarousel.getSlickDots().get(0)),
+                expectedActiveDotColor);
 
         //check other slickDots color are not orange
-        softAssert.assertTrue(carouselCardComponent.getSlickDots().stream()
-                        .filter(s -> carouselCardComponent.getSlickDotColor(s).equals(expectedActiveSlickDotColor))
-                        .count() == 1);
+        softAssert.assertTrue(clubCarousel.getSlickDots().stream()
+                .filter(s -> clubCarousel.getSlickDotColor(s).equals(expectedActiveDotColor))
+                .count() == 1);
 
         //check slickDot y alignment
-        int slickDotYLocation = carouselCardComponent.getActiveSlickDot().getLocation().y;
-        softAssert.assertTrue(carouselCardComponent.getSlickDots().stream().skip(1).allMatch(s -> s.getLocation().y == slickDotYLocation));
+        int slickDotYLocation = clubCarousel.getActiveSlickDot().getLocation().y;
+        softAssert.assertTrue(clubCarousel.getSlickDots().stream()
+                .skip(1)
+                .allMatch(s -> s.getLocation().y == slickDotYLocation));
 
         //click second slickDot
-        carouselCardComponent.clickSlickDotByIndex(1);
+        clubCarousel.clickSlickDotByIndex(1);
         //check color of the second slickDot is orange
-        softAssert.assertEquals(carouselCardComponent.getSlickDotColor(carouselCardComponent.getSlickDotByIndex(1)),
-                expectedActiveSlickDotColor);
+        softAssert.assertEquals(clubCarousel.getSlickDotColor(clubCarousel.getSlickDotByIndex(1)),
+                expectedActiveDotColor);
 
         //check other slickDots color are not orange
-        softAssert.assertTrue(carouselCardComponent.getSlickDots().stream()
-                .filter(s -> carouselCardComponent.getSlickDotColor(s).equals(expectedActiveSlickDotColor))
+        softAssert.assertTrue(clubCarousel.getSlickDots().stream()
+                .filter(s -> clubCarousel.getSlickDotColor(s).equals(expectedActiveDotColor))
                 .count() == 1);
         //check active carousel item indicator with suggested clubs correspond to the active blocks on the carousel
-        softAssert.assertEquals(carouselCardComponent.getActiveCarouselCards().stream()
-                                .map(c -> c.getClubCardHeading().getText()).collect(Collectors.toList()), carouselCardTitleExpected);
+        softAssert.assertEquals(clubCarousel.getActiveCarouselCards().stream()
+                .map(c -> c.getClubCardHeading().getText())
+                .collect(Collectors.toList()), expectedClubTitles);
 
         softAssert.assertAll();
     }

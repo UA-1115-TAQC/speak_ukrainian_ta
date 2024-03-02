597ff7647d67af6a2503e4ce049f3c3e38832bc4
Yuliia Ukrainets <ulaukrainec@gmail.com>
2024-03-02 18:14:00 +0200

    Finished doing the test

--- src/test/java/com/academy/ui/runners/HomePageTestRunner.java
+++ src/test/java/com/academy/ui/runners/HomePageTestRunner.java
@@ -4,6 +4,7 @@ import com.academy.ui.pages.HomePage;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 
@@ -11,10 +12,12 @@ public class HomePageTestRunner extends BaseTestRunner{
    protected HomePage home;
    protected WebDriverWait wait;
    protected String initialUrl;
+  protected SoftAssert softAssert;
     @BeforeMethod
     public void open_homepage(){
         home = new HomePage(driver);
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         initialUrl =driver.getCurrentUrl();
+        softAssert = new SoftAssert();
     }
 }

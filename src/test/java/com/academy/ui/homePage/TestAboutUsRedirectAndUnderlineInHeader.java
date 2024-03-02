87d87eb036cbcb67bd8a16ac385516e38c62b21f
Yuliia Ukrainets <ulaukrainec@gmail.com>
2024-03-02 15:44:14 +0200

    Added messages for soft asserts' '

--- src/test/java/com/academy/ui/homePage/TestAboutUsRedirectAndUnderlineInHeader.java
+++ src/test/java/com/academy/ui/homePage/TestAboutUsRedirectAndUnderlineInHeader.java
@@ -11,13 +11,15 @@ public class TestAboutUsRedirectAndUnderlineInHeader extends HomePageTestRunner 
     public void testAboutUsRedirectAndUnderlineInHeader(){
         wait.until(ExpectedConditions.elementToBeClickable(home.header.getAboutUsButton()));
         actions.moveToElement(home.header.getAboutUsButton()).build().perform();
-        softAssert.assertTrue(home.header.getAboutUsButtonContainer().getCssValue("border-bottom").contains("rgb(255, 255, 255)"));
+        softAssert.assertTrue(home.header.getAboutUsButtonContainer().getCssValue("border-bottom").contains("rgb(255, 255, 255)"),
+                "The about us container element isn't underlined after a mouse hover");
         AboutUsPage aboutUsPage = home.header.clickAboutUsButton();
         wait.until(ExpectedConditions.visibilityOf(aboutUsPage.getBannerFirstRowText()));
-        softAssert.assertTrue(driver.getCurrentUrl().contains("about"));
+        softAssert.assertTrue(driver.getCurrentUrl().contains("about"),"The AboutUs page isn't opened");
        home = aboutUsPage.header.clickTeachInUkrainianLogo();
        wait.until(ExpectedConditions.not(ExpectedConditions.urlContains("about")));
-        softAssert.assertTrue(home.header.getAboutUsButtonContainer().getCssValue("border-bottom").contains("0px none rgba(0, 0, 0, 0.88)"));
+        softAssert.assertTrue(home.header.getAboutUsButtonContainer().getCssValue("border-bottom").contains("0px none rgba(0, 0, 0, 0.88)"),
+                "The about us container element is still underlined after opening a home page");
         softAssert.assertAll();
     }
 }

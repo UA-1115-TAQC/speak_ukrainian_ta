283da3d54073496197978b9abec64fc0b66fca17
Nataliia Lazorko <natalia.lazorko.45@gmail.com>
2024-03-03 15:21:38 +0200

    TUA-867 UploadPhotoLinks and tooltip is Displayed

---
+++ src/test/java/com/academy/ui/profilePage/EditProfileWithManager.java
@@ -0,0 +1,32 @@
+package com.academy.ui.profilePage;
+
+import com.academy.ui.components.EditProfilePopUp;
+import com.academy.ui.pages.ProfilePage;
+import com.academy.ui.runners.LoginWithUserTestRunner;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class EditProfileWithManager extends LoginWithUserTestRunner {
+    private SoftAssert softAssert;
+
+    @BeforeMethod
+    public void SetUp() {
+        softAssert = new SoftAssert();
+    }
+
+    @Test(description = "TUA-867")
+    public void testUploadPhotoLinksAndTooltipIsDisplayed() {
+        homePage.header.openUserMenu().clickProfile();
+        EditProfilePopUp editProfile = new ProfilePage(driver).openEditUserProfile();
+        softAssert.assertTrue(editProfile.getPhotoLink().isDisplayed());
+
+        String actualResult = editProfile.getTooltipText();
+        softAssert.assertEquals(actualResult, "Приймас зображення формату JPG / PNG із мінімальною роздільною здатністю 200x200 пікселів та максимальним розміром файлу 5МВ", "Error messages are different");
+        softAssert.assertTrue(editProfile.getPhotoToolTipForm().isDisplayed());
+
+        softAssert.assertTrue(editProfile.getUploadPhotoLink().isDisplayed());
+        softAssert.assertAll();
+    }
+
+}

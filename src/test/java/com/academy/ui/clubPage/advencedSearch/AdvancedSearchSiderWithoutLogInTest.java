6af6a59d6de09946fc0574333033696f0f24c8ae
Oksanka Syrota <oksanochka.v@gmail.com>
2024-03-03 12:48:54 +0200

    Added TUA-858 test for AdvancedSearchSider without log in

--- src/test/java/com/academy/ui/clubPage/advencedSearch/AdvancedSearchSiderWithoutLogInTest.java
+++ src/test/java/com/academy/ui/clubPage/advencedSearch/AdvancedSearchSiderWithoutLogInTest.java
@@ -1,14 +1,21 @@
 package com.academy.ui.clubPage.advencedSearch;
 
-import com.academy.ui.components.AddLocationPopUpComponent.DropdownElement;
 import com.academy.ui.components.AdvancedSearchSiderComponent;
+import com.academy.ui.components.ClubInfoPopUp;
 import com.academy.ui.components.elements.LocationSearchSiderElement;
 import com.academy.ui.pages.ClubCardComponent;
 import com.academy.ui.pages.ClubsPage;
+import com.academy.ui.pages.DirectionTagComponent;
 import com.academy.ui.runners.BaseTestRunner;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AdvancedSearchSiderWithoutLogInTest extends BaseTestRunner {
 
@@ -23,8 +30,8 @@ public class AdvancedSearchSiderWithoutLogInTest extends BaseTestRunner {
         softAssert = new SoftAssert();
     }
 
-    @Test (description = "TUA-245")
-    public void verifyUserClubSearchCertainLocationByCityParameter(){
+    @Test(description = "TUA-245")
+    public void verifyUserClubSearchCertainLocationByCityParameter() {
         final String DEFAULT_CITY = "Київ";
         final String SELECTED_CITY = "Харків";
         softAssert.assertTrue(advancedSearchSider.getWebElement().isDisplayed(),
@@ -33,7 +40,7 @@ public class AdvancedSearchSiderWithoutLogInTest extends BaseTestRunner {
         String cityName = dropdownCity.getInputContent().getText();
         softAssert.assertEquals(cityName, DEFAULT_CITY);
         clubsPage.waitUntilClubsPageIsLoaded(5);
-        for (ClubCardComponent clubCard : clubsPage.getClubCards()){
+        for (ClubCardComponent clubCard : clubsPage.getClubCards()) {
             softAssert.assertTrue(clubCard.getAddress().getText().contains(DEFAULT_CITY),
                     "Address in the Card should contain selected city " + cityName);
         }
@@ -43,10 +50,82 @@ public class AdvancedSearchSiderWithoutLogInTest extends BaseTestRunner {
         String newCityName = dropdownCity.getInputContent().getText();
         softAssert.assertEquals(newCityName, SELECTED_CITY);
         clubsPage = clubsPage.waitUntilClubsPageIsLoaded(5);
-        for (ClubCardComponent clubCard : clubsPage.getClubCards()){
+        for (ClubCardComponent clubCard : clubsPage.getClubCards()) {
             softAssert.assertTrue(clubCard.getAddress().getText().contains(SELECTED_CITY),
                     "Address in the Card should contain selected city " + newCityName);
         }
         softAssert.assertAll();
     }
+
+    @Test(description = "TUA-858")
+    public void checkUserCanMakeSearchWith_1_Parameter() {
+        final String SELECTED_CITY = "Харків";
+        final String DEFAULT_CITY = "Київ";
+        final String CATEGORY = "Спортивні секції";
+        final String AGE = "7";
+        final int ELEMENT_NUMBER_IN_CARD_WITH_ONLINE = 7;
+        softAssert.assertTrue(advancedSearchSider.getWebElement().isDisplayed(),
+                "AdvancedSearchSider should be displayed");
+        LocationSearchSiderElement dropdownCity = advancedSearchSider.getSearchCityElement();
+        softAssert.assertFalse(dropdownCity.clickDropDown().getDropDownElement().getItemsList().isEmpty(),
+                "City dropdown shouldn't be empty");
+        dropdownCity.selectItem(SELECTED_CITY);
+        String cityName = dropdownCity.getInputContent().getText();
+        softAssert.assertEquals(cityName, SELECTED_CITY, "Dropdown value should be " + SELECTED_CITY);
+
+        clubsPage = new ClubsPage(driver).waitUntilClubsPageIsLoaded(5);
+        for (ClubCardComponent clubCard : clubsPage.getClubCards()) {
+            softAssert.assertTrue(clubCard.getAddress().getText().contains(SELECTED_CITY),
+                    "Address in the Card should contain selected city " + cityName);
+        }
+        dropdownCity.clickDropDown().selectItem(DEFAULT_CITY);
+
+        clubsPage = clubsPage.getSearchSider().checkOnlineCheckBox().waitUntilClubsPageIsLoaded(5);
+        for (ClubCardComponent clubCard : clubsPage.getClubCards()) {
+
+            softAssert.assertTrue(clubCard.getCardChildList().size() == ELEMENT_NUMBER_IN_CARD_WITH_ONLINE,
+                    "Club " + clubCard.getTitle().getText() + " should contain label \"Гурток онлайн\"");
+        }
+        clubsPage = clubsPage.getSearchSider().checkOnlineCheckBox().waitUntilClubsPageIsLoaded(10);
+
+        int countOnlineClubs = 0;
+        for (ClubCardComponent card : clubsPage.getClubCards()) {
+            if (card.getCardChildList().size() == ELEMENT_NUMBER_IN_CARD_WITH_ONLINE) {
+                countOnlineClubs++;
+            }
+        }
+        softAssert.assertNotEquals(clubsPage.getClubCards().size(), countOnlineClubs,
+                "Cards list should restore all cards after filter with OnlineClubs is off");
+
+        clubsPage = clubsPage.getSearchSider().checkDirectionCheckBox(CATEGORY).waitUntilClubsPageIsLoaded(5);
+        for (ClubCardComponent clubCard : clubsPage.getClubCards()) {
+            List<String> list = new ArrayList<>();
+            for (DirectionTagComponent direction : clubCard.getDirections()) {
+                list.add(direction.getName().getText());
+            }
+            softAssert.assertTrue(list.contains(CATEGORY),
+                    "Club " + clubCard.getTitle().getText() + " should contain category " + CATEGORY);
+        }
+        clubsPage = clubsPage.getSearchSider().checkDirectionCheckBox(CATEGORY).waitUntilClubsPageIsLoaded(5);
+        int countCategories = 0;
+        for (ClubCardComponent clubCard : clubsPage.getClubCards()) {
+            countCategories += clubCard.getListOfDirectionsTitles().contains(CATEGORY) ? 1 : 0;
+        }
+        softAssert.assertNotEquals(clubsPage.getClubCards().size(), countCategories,
+                "Cards list should restore all cards after filter with Category is off");
+
+        clubsPage = clubsPage.getSearchSider().enterAge(AGE).waitUntilClubsPageIsLoaded(5);
+        ;
+        int currentAge = Integer.parseInt(AGE);
+        for (ClubCardComponent clubCard : clubsPage.getClubCards()) {
+            ClubInfoPopUp clubInfoPopUp = clubCard.clickTitle();
+            List<Integer> age = clubInfoPopUp.getClubsAgeList();
+            softAssert.assertTrue(currentAge >= age.get(0) && currentAge <= age.get(1),
+                    "Age " + AGE + " should be in a range from " + age.get(0) + " to " + age.get(1));
+            clubInfoPopUp.getCloseButton().click();
+        }
+
+        softAssert.assertAll();
+    }
+
 }

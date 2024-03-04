5962fd78c728da071a06aefde2a8233f80d7d073
Oksanka Syrota <oksanochka.v@gmail.com>
2024-03-05 00:08:58 +0200

    Added methods for the AddCenterPopUpStepThree

--- src/main/java/com/academy/ui/components/AddCenterPopUPComponent/AddCenterPopUpStepThree.java
+++ src/main/java/com/academy/ui/components/AddCenterPopUPComponent/AddCenterPopUpStepThree.java
@@ -1,16 +1,21 @@
 package com.academy.ui.components.AddCenterPopUPComponent;
 
+import com.academy.ui.components.AddClubPopUpComponent.AddClubPopUpStepThree;
 import com.academy.ui.components.AddClubPopUpComponent.UploadedImgComponent;
 import lombok.AccessLevel;
 import lombok.Getter;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-public class AddCenterPopUpStepThree extends AddCenterPopUpContainer{
+public class AddCenterPopUpStepThree extends AddCenterPopUpContainer {
 
     @FindBy(xpath = "./descendant::span[contains(@class,'ant-typography')][1]")
     private WebElement centerLogoTitle;
@@ -57,6 +62,31 @@ public class AddCenterPopUpStepThree extends AddCenterPopUpContainer{
         super(driver, rootElement);
         uploadedCoverImg = new UploadedImgComponent(driver, uploadedCoverImgContainer);
         uploadedLogoImg = new UploadedImgComponent(driver, uploadedLogoImgContainer);
+    }
+
+    public AddCenterPopUpStepThree setCenterDescriptionTextarea(String text) {
+        centerDescriptionTextarea.sendKeys(text);
+        return this;
+    }
+
+    public void clickCompleteButton(){
+        getNextStepButton().click();
+    }
+
+    public List<String> getErrorMessagesTextList() {
+        return errorMessagesTextarea.stream()
+                .map(elem -> elem.getAttribute("innerText"))
+                .collect(Collectors.toList());
+    }
+
+    public AddCenterPopUpStepThree clearDescriptionTextarea(){
+        Platform currentPlatform = ((RemoteWebDriver) driver).getCapabilities().getPlatformName();
+        if (currentPlatform.is(Platform.MAC)) {
+            centerDescriptionTextarea.sendKeys(Keys.COMMAND + "a", Keys.DELETE);
+        } else {
+            centerDescriptionTextarea.sendKeys(Keys.CONTROL + "a", Keys.BACK_SPACE);
+        }
+        return this;
     }
 
 }

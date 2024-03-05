a40b9ec5ff148a66e79c394ab497fdd9075e6e4c
Anastasiia <anastasiiak55@gmail.com>
2024-03-05 17:11:21 +0200

    added chalange pages check in checkLowerPartOfHeaderOnPages method

--- src/test/java/com/academy/ui/header/HeaderComponentTest.java
+++ src/test/java/com/academy/ui/header/HeaderComponentTest.java
@@ -1,5 +1,6 @@
 package com.academy.ui.header;
 
+import com.academy.ui.components.header.HeaderChallengesDropdown;
 import com.academy.ui.components.header.HeaderComponent;
 import com.academy.ui.pages.*;
 import com.academy.ui.pages.challenges.BaseChallengePage;
@@ -7,6 +8,8 @@ import com.academy.ui.runners.BaseTestRunner;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import java.util.List;
 
 public class HeaderComponentTest extends BaseTestRunner {
     private HeaderComponent header;
@@ -28,7 +31,6 @@ public class HeaderComponentTest extends BaseTestRunner {
         softAssert.assertTrue(home.getAdvancedSearchHeaderComponentNode().isDisplayed());
         header = home.getHeader();
 
-
         AllNewsPage allNewsPage = header.newsButtonClick();
         softAssert.assertFalse(allNewsPage.getAdvancedSearchHeaderComponentNode().isDisplayed());
         header = allNewsPage.getHeader();
@@ -41,9 +43,13 @@ public class HeaderComponentTest extends BaseTestRunner {
         softAssert.assertFalse(servicePage.getAdvancedSearchHeaderComponentNode().isDisplayed());
         header = servicePage.getHeader();
 
-
-        BaseChallengePage p = header.challengesClick();
-//       TODO  chalanges
+        HeaderChallengesDropdown challengesDropdown = header.clickChallengeButton();
+        for(int i = 0; i < challengesDropdown.getChallengeDropdownItems().size(); i++){
+            BaseChallengePage challengePage = challengesDropdown.clickChallengeDropdownItemByIndex(i);
+            softAssert.assertFalse(challengePage.getAdvancedSearchHeaderComponentNode().isDisplayed());
+            header = challengePage.getHeader();
+            challengesDropdown = header.clickChallengeButton();
+        }
 
         softAssert.assertAll();
     }

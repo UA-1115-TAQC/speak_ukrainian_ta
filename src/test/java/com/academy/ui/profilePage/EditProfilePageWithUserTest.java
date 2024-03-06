2c2b18e917456dcbfe57dd25ea0c23acd52d5bf1
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-03-06 14:00:51 +0200

    tua-421
    editing profile with invalid phone should fail

--- src/test/java/com/academy/ui/profilePage/EditProfilePageWithUserTest.java
+++ src/test/java/com/academy/ui/profilePage/EditProfilePageWithUserTest.java
@@ -2,12 +2,14 @@ package com.academy.ui.profilePage;
 
 import com.academy.ui.components.EditProfilePopUp;
 import com.academy.ui.pages.ProfilePage;
-
 import com.academy.ui.runners.LogInWithUserTestRunner;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class EditProfilePageWithUserTest extends LogInWithUserTestRunner {
     private SoftAssert softAssert;
@@ -53,6 +55,39 @@ public class EditProfilePageWithUserTest extends LogInWithUserTestRunner {
         softAssert.assertAll();
     }
 
+    @Test(description = "TUA-421", dataProvider = "invalidPhone")
+    public void checkEditPhoneFieldWithInvalidData(String phone, String[] expectedErrorMsg) throws InterruptedException {
+        final var emptyFieldErrorMsg = "Будь ласка введіть Ваш номер телефону";
+
+        var editProfilePopUp = profilePage.openEditUserProfile();
+        editProfilePopUp.waitPopUpOpen(10);
+        softAssert.assertTrue(editProfilePopUp.getSubmitButton().isEnabled(),
+                "SubmitButton should be enabled");
+
+        System.out.println(phone);
+        System.out.println(Arrays.toString(expectedErrorMsg));
+
+        var phoneElement = editProfilePopUp.getPhoneElement();
+        System.out.println(phoneElement);
+        phoneElement.clearInput();
+        List<String> errorMessagesTextList = phoneElement.getErrorMessagesTextList();
+
+        System.out.println(Arrays.toString(errorMessagesTextList.toArray()));
+
+        softAssert.assertEquals(errorMessagesTextList.get(0), emptyFieldErrorMsg);
+        softAssert.assertFalse(editProfilePopUp.getSubmitButton().isEnabled(),
+                "Submit button should not be enabled");
+
+        phoneElement.setValue(phone);
+        for (int i = 0; i < expectedErrorMsg.length; i++) {
+            softAssert.assertEquals(phoneElement.getErrorMessagesTextList().get(i), expectedErrorMsg[i]);
+            softAssert.assertFalse(editProfilePopUp.getSubmitButton().isEnabled(),
+                    "Submit button should not be enabled");
+        }
+
+        softAssert.assertAll();
+    }
+
     @DataProvider(name = "invalidFirstName")
     private Object[][] invalidFirstNameDataProvider() {
         return new Object[][] {
@@ -66,6 +101,17 @@ public class EditProfilePageWithUserTest extends LogInWithUserTestRunner {
                 {"Name-", "Ім'я повинно починатися та закінчуватися літерою"},
                 {"Name ", "Ім'я не може містити спеціальні символи"},
                 {"Name'", "Ім'я повинно починатися та закінчуватися літерою"}
+        };
+    }
+
+    @DataProvider(name = "invalidPhone")
+    private Object[][] invalidPhoneDataProvider() {
+        return new Object[][] {
+                {"a", new String[] {"Телефон не може містити літери", "Телефон не відповідає вказаному формату"}},
+                {"-a", new String[] {"Телефон не може містити літери", "Телефон не відповідає вказаному формату", "Телефон не може містити спеціальні символи"}},
+                {";", new String[] {"Телефон не відповідає вказаному формату", "Телефон не може містити спеціальні символи"}},
+                {"99999999999999999", new String[] {"Телефон не відповідає вказаному формату"}},
+                {" m", new String[] {"Телефон не може містити літери", "Телефон не може містити пробіли", "Телефон не відповідає вказаному формату", "Телефон не може містити спеціальні символи"}},
         };
     }
 

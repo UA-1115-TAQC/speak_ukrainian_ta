cf951ed9229cd1e9da0bad812981b56c734aa622
Alevtyna Parysta <luxor952@gmail.com>
2024-03-06 20:25:29 +0100

    refactoring expected message

--- src/test/java/com/academy/ui/homePage/TestCarouselItemIndicatorCorrespondToActiveBlocks.java
+++ src/test/java/com/academy/ui/homePage/TestCarouselItemIndicatorCorrespondToActiveBlocks.java
@@ -10,22 +10,22 @@ import java.util.List;
 
 public class TestCarouselItemIndicatorCorrespondToActiveBlocks extends HomePageTestRunner {
 
-    private static final String ACTIVE_EXPECTED_COLOR = "#fa8c16";
-    private static final String INACTIVE_EXPECTED_COLOR = "#c4c4c4";
+    private static final String EXPECTED_ACTIVE_HEX_COLOR = "#fa8c16";
+    private static final String EXPECTED_INACTIVE_HEX_COLOR = "#c4c4c4";
 
     @Test(description = "TUA-829")
     public void checkThatCarouselIndicatorWithSuggestedClubsCorrespondToActiveBlocks() {
         homePage.carouselCardComponent.clickSlickDotByIndex(0);
         String activeDot = homePage.carouselCardComponent.getActiveSlickDot().getCssValue("background-color");
-        String hexColor = Color.fromString(activeDot).asHex();
-        softAssert.assertEquals(hexColor, ACTIVE_EXPECTED_COLOR, "Button color is not as expected");
+        String actualHexColor = Color.fromString(activeDot).asHex();
+        softAssert.assertEquals(actualHexColor, EXPECTED_ACTIVE_HEX_COLOR, "Button color is not as expected");
 
 
         String inActiveDot = homePage.carouselCardComponent.getSlickDotByIndex(2).
                 getCssValue("background-color");
 
-        String hexColor2 = Color.fromString(inActiveDot).asHex();
-        softAssert.assertEquals(hexColor2, INACTIVE_EXPECTED_COLOR, "Button color is not as expected");
+        String actualInactiveHexColor = Color.fromString(inActiveDot).asHex();
+        softAssert.assertEquals(actualInactiveHexColor, EXPECTED_INACTIVE_HEX_COLOR, "Button color is not as expected");
 
         List<ClubDirectionCard> clubAtFirstPage = homePage.carouselCardComponent.getActiveCarouselCards();
         homePage.carouselCardComponent.clickSlickDotByIndex(1);

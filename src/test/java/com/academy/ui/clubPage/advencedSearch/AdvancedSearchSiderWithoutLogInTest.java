5b4789946fa89a7e1bec7ebf6015bcfdfcb2a88e
Alevtyna Parysta <luxor952@gmail.com>
2024-03-06 13:48:33 +0100

    added test

--- src/test/java/com/academy/ui/clubPage/advencedSearch/AdvancedSearchSiderWithoutLogInTest.java
+++ src/test/java/com/academy/ui/clubPage/advencedSearch/AdvancedSearchSiderWithoutLogInTest.java
@@ -6,6 +6,7 @@ import com.academy.ui.components.elements.LocationSearchSiderElement;
 import com.academy.ui.pages.ClubCardComponent;
 import com.academy.ui.pages.ClubsPage;
 import com.academy.ui.runners.BaseTestRunner;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -23,8 +24,8 @@ public class AdvancedSearchSiderWithoutLogInTest extends BaseTestRunner {
         softAssert = new SoftAssert();
     }
 
-    @Test (description = "TUA-245")
-    public void verifyUserClubSearchCertainLocationByCityParameter(){
+    @Test(description = "TUA-245")
+    public void verifyUserClubSearchCertainLocationByCityParameter() {
         final String DEFAULT_CITY = "Київ";
         final String SELECTED_CITY = "Харків";
         softAssert.assertTrue(advancedSearchSider.getWebElement().isDisplayed(),
@@ -33,7 +34,7 @@ public class AdvancedSearchSiderWithoutLogInTest extends BaseTestRunner {
         String cityName = dropdownCity.getInputContent().getText();
         softAssert.assertEquals(cityName, DEFAULT_CITY);
         clubsPage.waitUntilClubsPageIsLoaded(5);
-        for (ClubCardComponent clubCard : clubsPage.getClubCards()){
+        for (ClubCardComponent clubCard : clubsPage.getClubCards()) {
             softAssert.assertTrue(clubCard.getAddress().getText().contains(DEFAULT_CITY),
                     "Address in the Card should contain selected city " + cityName);
         }
@@ -43,10 +44,26 @@ public class AdvancedSearchSiderWithoutLogInTest extends BaseTestRunner {
         String newCityName = dropdownCity.getInputContent().getText();
         softAssert.assertEquals(newCityName, SELECTED_CITY);
         clubsPage = clubsPage.waitUntilClubsPageIsLoaded(5);
-        for (ClubCardComponent clubCard : clubsPage.getClubCards()){
+        for (ClubCardComponent clubCard : clubsPage.getClubCards()) {
             softAssert.assertTrue(clubCard.getAddress().getText().contains(SELECTED_CITY),
                     "Address in the Card should contain selected city " + newCityName);
         }
         softAssert.assertAll();
     }
+
+    @Test(description = "TUA-259-Verify that user can find club using all parameters of advanced search")
+    public void checkIfUserCanFindClubUsingAdvancedSearch() {
+        advancedSearchSider.getSearchDistrictElement().clickDropDown().selectItem("Оболонський");
+        advancedSearchSider.getSearchMetroElement().clickDropDown().selectItem("Почайна");
+        advancedSearchSider.getOnlineCheckBox().click();
+        advancedSearchSider.checkDirectionCheckBox("Програмування, робототехніка, STEM");
+        advancedSearchSider.enterAge("8");
+
+        softAssert.assertTrue(clubsPage.getClubCards().stream()
+                .flatMap(component -> component.getDirectionTags().stream())
+                .anyMatch(component -> component.getText().contains("Програмування, робототехніка, STEM")));
+    }
+
+
 }
+

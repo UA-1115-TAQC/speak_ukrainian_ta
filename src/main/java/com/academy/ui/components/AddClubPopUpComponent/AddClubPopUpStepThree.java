42bb44c5b716f5670c48bd4a0fdd30739303b9af
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2024-03-09 13:30:45 +0200

    fixed xpaths for valid work of tests

--- src/main/java/com/academy/ui/components/AddClubPopUpComponent/AddClubPopUpStepThree.java
+++ src/main/java/com/academy/ui/components/AddClubPopUpComponent/AddClubPopUpStepThree.java
@@ -45,7 +45,7 @@ public class AddClubPopUpStepThree extends AddClubPopUpContainer{
     @FindBy(xpath = "./descendant::span[(@class='ant-upload') and (@role='button')][3]//input")
     private WebElement clubGalleryDownloadInput;
 
-    @FindBy(xpath = "./descendant::textarea[@id='basic_descriptionText']")
+    @FindBy(xpath = "./descendant::textarea[(@id='basic_descriptionText') or (@id='basic_description')]")
     private WebElement clubDescriptionTextarea;
 
     @FindBy(xpath = ".//span[contains(@class, 'ant-form-item-feedback-icon')]")

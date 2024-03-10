7d03477b9208bfa172abadbe2225ce923d916076
Alevtyna Parysta <luxor952@gmail.com>
2024-03-10 15:19:11 +0100

    added test

--- src/test/java/com/academy/ui/addCenter/TestAddLocationToCenterPopUp.java
+++ src/test/java/com/academy/ui/addCenter/TestAddLocationToCenterPopUp.java
@@ -2,6 +2,7 @@ package com.academy.ui.addCenter;
 
 import com.academy.ui.components.AddCenterPopUpComponent.*;
 import com.academy.ui.components.AddLocationPopUpComponent.AddLocationPopUpComponent;
+import com.academy.ui.pages.ProfilePage;
 import com.academy.ui.runners.LoginWithManagerTestRunner;
 import io.qameta.allure.Issue;
 import org.testng.annotations.BeforeMethod;
@@ -24,13 +25,11 @@ public class TestAddLocationToCenterPopUp extends LoginWithManagerTestRunner {
 
     private static final String TELEPHONE_NUMBER = "0976756754";
 
-
+    private static final String DESCRIPTION = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. It has survived not only five centuries.";
+    private static final String CLUB = "Study with us";
 
     private AddCenterPopUpComponent addCenterPopUp;
     private AddCenterPopUpStepOne stepOne;
-    private AddCenterPopUpStepTwo stepTwo;
-    private AddCenterPopUpStepThree stepThree;
-    private AddCenterPopUpStepFour stepFour;
     private SoftAssert softAssert;
 
     @BeforeMethod
@@ -43,7 +42,7 @@ public class TestAddLocationToCenterPopUp extends LoginWithManagerTestRunner {
 
     @Test(description = "Verify that a 'Керівник' can add a location of a center")
     @Issue("TUA-247")
-    public void addLocationOfACenterTest(){
+    public void addLocationOfACenterTest() throws InterruptedException {
 
         stepOne.setCenterName(CENTER);
         AddLocationPopUpComponent addLocationPopUp = stepOne.clickAddLocationButton();
@@ -72,23 +71,23 @@ public class TestAddLocationToCenterPopUp extends LoginWithManagerTestRunner {
         stepOne.clickLocationCheckboxByName(LOCATION);
         stepOne.clickNextStepButton();
 
+        AddCenterPopUpStepTwo stepTwo = addCenterPopUp.getStepTwoContainer();
 
+        stepTwo.getTelephoneInputElement().setValue(TELEPHONE_NUMBER);
+        softAssert.assertEquals(stepTwo.getTelephoneInputElement().getInput().getAttribute("value"), TELEPHONE_NUMBER);
+        stepTwo.clickNextStepButton();
+
+        AddCenterPopUpStepThree stepThree = addCenterPopUp.getStepThreeContainer();
+        stepThree.setCenterDescriptionTextarea(DESCRIPTION);
+        softAssert.assertEquals(stepThree.getCenterDescriptionTextarea().getText(), DESCRIPTION);
+        stepThree.clickNextStepButton();
+
+        AddCenterPopUpStepFour stepFour = addCenterPopUp.getStepFourContainer();
+        stepFour.clickOnClubCheckBoxByName(CLUB);
+        stepFour.clickFinishButton();
 
         softAssert.assertAll();
 
-
-
-
-
-
-
-
-
-
-
     }
-
-
-
 
 }

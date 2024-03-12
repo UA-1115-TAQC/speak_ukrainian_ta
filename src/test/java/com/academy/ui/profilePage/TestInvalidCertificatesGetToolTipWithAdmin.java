9d8d3bdb2cb9b638bfa7777fa10d8a03552281ea
Yuliia Ukrainets <ulaukrainec@gmail.com>
2024-03-12 11:40:47 +0200

    Finished implementing the test

--- src/test/java/com/academy/ui/profilePage/TestInvalidCertificatesGetToolTipWithAdmin.java
+++ src/test/java/com/academy/ui/profilePage/TestInvalidCertificatesGetToolTipWithAdmin.java
@@ -2,15 +2,15 @@ package com.academy.ui.profilePage;
 
 import com.academy.ui.components.header.headerMenuComponent.adminpopup.submenu.CertificatesSubmenuPopup;
 import com.academy.ui.pages.AdminGenerateCertificatePage;
-import com.academy.ui.pages.ProfilePage;
 import com.academy.ui.runners.LoginWithAdminTestRunner;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
-import java.security.cert.Certificate;
+import java.time.Duration;
 
 public class TestInvalidCertificatesGetToolTipWithAdmin extends LoginWithAdminTestRunner {
     SoftAssert softAssert;
@@ -18,21 +18,29 @@ public class TestInvalidCertificatesGetToolTipWithAdmin extends LoginWithAdminTe
     AdminGenerateCertificatePage adminGenerateCertificatePage;
     String minValidValue = "1";
     String maxValidValue = "999";
+    WebDriverWait wait;
     @BeforeMethod
     public void setup(){
         softAssert = new SoftAssert();
         adminGenerateCertificatePage = homePage.header.openAdminMenu().openContentPopup().openCertificatesSubmenuPopup().clickGenerateCertificate();
-
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+    }
+    @DataProvider(name = "invalidValues")
+    public Object[][] invalidValues() {
+        return new Object[][] {
+                {"1000", maxValidValue},
+                {"1300", maxValidValue},
+                {"0", minValidValue}
+        };
     }
 
-    @Test
-    public void testInvalidCertificatesGetToolTip(){
-       /* clearInputField(adminGenerateCertificatePage.getStudyDurationInput());
-        adminGenerateCertificatePage.getStudyDurationInput().sendKeys("1000");*/
-        enterInValidValue("1000");
+    @Test(dataProvider = "invalidValues")
+    public void testInvalidCertificatesGetToolTip(String inputValue, String expectedValue){
+        enterInValidValue(inputValue);
         adminGenerateCertificatePage.getStudyDurationLabel().click();
-        adminGenerateCertificatePage.sleep(1000);
-
+        wait.until(driver -> !adminGenerateCertificatePage.getStudyDurationInput().getAttribute("value").contains(inputValue));
+        softAssert.assertTrue(adminGenerateCertificatePage.getStudyDurationInput().getAttribute("value").contains(expectedValue),
+                "The duration input accepts an invalid value "+ inputValue);
         softAssert.assertAll();
     }
     private void clearInputField(WebElement inputField){
@@ -40,10 +48,17 @@ public class TestInvalidCertificatesGetToolTipWithAdmin extends LoginWithAdminTe
         inputField.sendKeys(Keys.DELETE);
     }
     private void enterInValidValue(String value){
-        if (!adminGenerateCertificatePage.getStudyDurationLabel().getAttribute("value").isEmpty()){
-            clearInputField(adminGenerateCertificatePage.getStudyDurationInput());
+        WebElement studyDurationInput = adminGenerateCertificatePage.getStudyDurationInput();
+        String currentValue = studyDurationInput.getAttribute("value");
+
+        if (currentValue != null && !currentValue.isEmpty()) {
+            clearInputField(studyDurationInput);
         }
-        adminGenerateCertificatePage.getStudyDurationInput().sendKeys(value);
-        softAssert.assertTrue(adminGenerateCertificatePage.getStudyDurationInput().getAttribute("value").contains(value));
+
+        studyDurationInput.sendKeys(value);
+
+        String updatedValue = studyDurationInput.getAttribute("value");
+        softAssert.assertTrue(updatedValue != null && updatedValue.contains(value),
+                "The entered value isn't present in the duration input");
     }
 }

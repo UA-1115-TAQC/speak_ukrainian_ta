4f99ccd30c1446d15dcd5d1d07d9a108f42b74be
Liubomyr Halamaha <galamagal86@gmail.com>
2024-03-13 13:03:52 +0200

    fix

--- src/main/java/com/academy/ui/components/AddClubPopUpComponent/AddClubPopUpStepThree.java
+++ src/main/java/com/academy/ui/components/AddClubPopUpComponent/AddClubPopUpStepThree.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;

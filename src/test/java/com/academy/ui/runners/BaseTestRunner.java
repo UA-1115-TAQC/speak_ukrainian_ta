08687141257e3c8ab364523e51f378f5c91dd50e
Liubomyr Halamaha <galamagal86@gmail.com>
2024-03-14 14:05:08 +0200

    add TestNgListener

--- src/test/java/com/academy/ui/runners/BaseTestRunner.java
+++ src/test/java/com/academy/ui/runners/BaseTestRunner.java
@@ -2,18 +2,21 @@ package com.academy.ui.runners;
 
 import com.academy.ui.pages.HomePage;
 import com.academy.ui.runners.utils.ConfigProperties;
-import com.academy.ui.runners.utils.ScreenShotUtil;
+import com.academy.ui.runners.utils.TestNgListener;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.ITestResult;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
 
 import java.time.Duration;
 
+
+@Listeners(TestNgListener.class)
 public abstract class BaseTestRunner {
     protected WebDriver driver;
     protected static ConfigProperties configProperties;
@@ -28,24 +31,21 @@ public abstract class BaseTestRunner {
     }
 
     @BeforeMethod(description = "Web browser configuration")
-    public void setUp() {
-//        if you want to use headless mode uncomment 3 lines bellow
+    public void setUp(ITestContext context) {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
-        driver = new ChromeDriver(options);
 //        if you want to use head mode uncomment one line below
-//        driver = new ChromeDriver();
+//        options.addArguments("--headless=new");
+        driver = new ChromeDriver(options);
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitlyWaitDuration));
+        context.setAttribute("webDriver", driver);
         driver.get(configProperties.getBaseUrl());
         homePage = new HomePage(driver);
     }
 
     @AfterMethod(description = "Post Condition of closing every associated window")
-    public void tearDown(ITestResult result) {
-        if (ITestResult.FAILURE == result.getStatus()) {
-            ScreenShotUtil.captureScreenshot(driver, result.getName());
-        }
+    public void tearDown() {
         if (driver != null) {
             driver.quit();
         }

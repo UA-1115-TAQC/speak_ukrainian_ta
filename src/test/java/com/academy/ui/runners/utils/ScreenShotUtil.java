323240b6c57552f71f9ee2ab39151c075796f91d
Oksanka Syrota <oksanochka.v@gmail.com>
2024-03-14 10:58:38 +0200

    Added allure attachment for the screenshot

--- src/test/java/com/academy/ui/runners/utils/ScreenShotUtil.java
+++ src/test/java/com/academy/ui/runners/utils/ScreenShotUtil.java
@@ -1,10 +1,12 @@
 package com.academy.ui.runners.utils;
 
+import io.qameta.allure.Allure;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 
@@ -16,6 +18,8 @@ public class ScreenShotUtil {
             TakesScreenshot ts = (TakesScreenshot) driver;
             File source = ts.getScreenshotAs(OutputType.FILE);
             FileUtils.copyFile(source, new File( screenshotPath + screenshotName + ".png"));
+            byte[] screenshotBytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            Allure.addAttachment(screenshotName, "image/png", new ByteArrayInputStream(screenshotBytes), ".png");
         } catch (IOException e) {
             throw new RuntimeException("Exception while taking screenshot ", e);
         }

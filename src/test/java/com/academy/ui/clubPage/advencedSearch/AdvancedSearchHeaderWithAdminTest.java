70b2cb3a0b14ca349301b985c8245b9ec9b39164
Zakotiuk <vzakotuk@gmail.com>
2024-03-20 17:31:31 +0200

    Done TUA-246

--- src/test/java/com/academy/ui/clubPage/advencedSearch/AdvancedSearchHeaderWithAdminTest.java
+++ src/test/java/com/academy/ui/clubPage/advencedSearch/AdvancedSearchHeaderWithAdminTest.java
@@ -1,13 +1,21 @@
 package com.academy.ui.clubPage.advencedSearch;
 
-import com.academy.ui.components.advancedSearchHeader.AdvancedSearchHeaderComponent;
+import com.academy.ui.components.CenterCardComponent;
 import com.academy.ui.pages.AllNewsPage;
+import com.academy.ui.pages.ClubsPage;
 import com.academy.ui.runners.LoginWithAdminTestRunner;
+import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import io.qameta.allure.Description;
 import io.qameta.allure.Issue;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+import java.util.List;
 
 public class AdvancedSearchHeaderWithAdminTest extends LoginWithAdminTestRunner {
 
@@ -37,5 +45,39 @@ public class AdvancedSearchHeaderWithAdminTest extends LoginWithAdminTestRunner 
 
         softAssert.assertAll();;
     }
+    @Test(description = "TUA-246")
+    public void advancedSearchCheckChangingClubsWithChangingTown(){
+        ClubsPage clubsPage = new ClubsPage(driver);
+        AllNewsPage allNewsPage = homePage.header.newsButtonClick();
 
+        allNewsPage.advancedSearchHeaderComponent.clickAdvancedSearchIcon();
+        softAssert.assertEquals(clubsPage.getSearchSider().getLabel().getText(), "Розширений пошук");
+
+        clubsPage = clubsPage.getSearchSider().chooseRadioButton("Центр");
+        softAssert.assertEquals(clubsPage.getSearchSider().getSearchCityElement().getInputContent().getText(), "Київ");
+
+        List<CenterCardComponent> centerCards = clubsPage.getCenterCards();
+        softAssert.assertTrue(centerCards.get(1).getAddress().getText().contains("Київ"));
+        softAssert.assertTrue(centerCards.get(4).getAddress().getText().contains("Київ"));
+        softAssert.assertTrue(centerCards.get(5).getAddress().getText().contains("Київ"));
+
+        clubsPage.getSearchSider().clickCityDropDown();
+        softAssert.assertTrue(clubsPage.getSearchSider().getSearchCityElement().getDropDownElement()
+                                                                                .getWebElement()
+                                                                                .isDisplayed());
+
+        clubsPage.getSearchSider().getSearchCityElement().selectItem("Харків");
+        softAssert.assertEquals(clubsPage.getSearchSider().getSearchCityElement().getInputContent()
+                                                                                    .getText(), "Харків");
+
+        clubsPage = new ClubsPage(driver);
+        centerCards = clubsPage.getCenterCards();
+
+        for (CenterCardComponent centerCard : centerCards) {
+            softAssert.assertTrue(centerCard.getAddress().getText().contains("Харків"),
+                                    "False location. Found locations : " + centerCard.getAddress().getText());
+        }
+
+        softAssert.assertAll();
+    }
 }
